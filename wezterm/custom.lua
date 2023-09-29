8215f2734bcbcfed09f325c2e6f8f353bd825f1a
Isabella Skořepová <isabella@skorepova.info>
2023-09-29 14:58:28 +0200

    term font

--- wezterm/custom.lua
+++ wezterm/custom.lua
@@ -6,6 +6,8 @@ local module = {}
 
 function module.apply_to_config(config)
   --config.color_scheme = 'Batman'
+  config.font = wezterm.font_with_fallback { 'FiraCode Nerd Font' }
+  config.font_size = 12;
 end
 
 -- return our module table
