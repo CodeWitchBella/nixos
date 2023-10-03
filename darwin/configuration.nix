089d79be22ed9de79f2d47f757bb5ab8c6199d04
Isabella Skořepová <isabella@skorepova.info>
2023-10-03 20:03:07 +0200

    harmonize git configs

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -55,10 +55,12 @@
       userName = "Isabella Skořepová";
       extraConfig = {
         init.defaultBranch = "main";
+        push.autoSetupRemote = true;
         submodule.recurse = "true";
-        commit = {
-          gpgSign = true;
-        };
+        user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEGS4V/SauPK+C9moGX5gscGYYPNV5E6QNUyaZrL1eg0";
+        commit.gpgsign = true;
+        gpg.format = "ssh";
+        "gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
         alias.frp = "!bash -c \"git fetch --prune ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`; git push --force\"";
         alias.fr = "!bash -c \"git fetch --prune ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`\"";
       };
--- nixos/home.nix
+++ nixos/home.nix
@@ -18,10 +18,13 @@
     extraConfig = {
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
+      submodule.recurse = "true";
       user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEGS4V/SauPK+C9moGX5gscGYYPNV5E6QNUyaZrL1eg0";
       commit.gpgsign = true;
       gpg.format = "ssh";
       "gpg \"ssh\"".program = "op-ssh-sign";
+      alias.frp = "!bash -c \"git fetch --prune ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`; git push --force\"";
+      alias.fr = "!bash -c \"git fetch --prune ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`\"";
     };
   };
   programs.ssh = {
