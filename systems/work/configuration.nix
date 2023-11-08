eb937d72ee320c002a8a9cea0bd0ef597fc44689
Isabella Skořepová <isabella@skorepova.info>
2023-11-08 01:09:59 +0100

    sleep attempt

--- systems/work/configuration.nix
+++ systems/work/configuration.nix
@@ -14,6 +14,10 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [
+    "mem_sleep_default=deep"
+  ];
+  services.fwupd.enable = true;
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
