e0f7c580b9557d27278e82bfcc558f36220ef15b
Isabella Skořepová <isabella@skorepova.info>
2023-11-08 18:02:25 +0100

    cursor hiding is a bit borked

--- wezterm/custom.lua
+++ wezterm/custom.lua
@@ -8,6 +8,7 @@ function module.apply_to_config(config)
   --config.color_scheme = 'Batman'
   config.font = wezterm.font_with_fallback { 'FiraCode Nerd Font' }
   config.font_size = 12;
+  config.hide_mouse_cursor_when_typing = false;
 end
 
 -- return our module table
