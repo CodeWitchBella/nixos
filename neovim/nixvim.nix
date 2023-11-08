001c6a2bc2f7d24fb09a477a54a6137b6dc2c98f
Isabella Skořepová <isabella@skorepova.info>
2023-11-08 15:13:46 +0100

    reasonable config

--- neovim/nixvim.nix
+++ neovim/nixvim.nix
@@ -1,11 +1,44 @@
 { pkgs }: {
     enable = true;
 
-    extraPlugins = [ pkgs.vimPlugins.gruvbox ];
-    colorscheme = "gruvbox";
-    plugins.lightline.enable = true;
-    plugins.telescope = {
-        enable = true;
+    extraPlugins = with pkgs.vimPlugins; [
+        catppuccin-nvim
+
+        # Git related plugins
+        fugitive
+        rhubarb
+
+        # Detect tabstop and shiftwidth automatically
+        vim-sleuth
+
+        # LSP
+        nvim-lspconfig
+
+        # Autocompletion
+        nvim-cmp
+
+        # Useful plugin to show you pending keybinds.
+        which-key-nvim
+
+        # Adds git related signs to the gutter, as well as utilities for managing changes
+        gitsigns-nvim
+
+        # Set lualine as statusline
+        lualine-nvim
+
+        # the rest
+        mason-nvim
+        mason-lspconfig-nvim
+        neodev-nvim
+        cmp-nvim-lsp
+        luasnip
+    ];
+    colorscheme = "catppuccin";
+    plugins = {
+        telescope.enable = true;
+
+        # Highlight, edit, and navigate code
+        treesitter.enable = true;
     };
     plugins.lsp.servers.tsserver.enable = true;
     extraConfigLua = "dofile('/home/isabella/nixos/neovim/init.lua')";
