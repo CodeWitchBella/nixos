9570a4ba45d6c4211badb99682a683d4a141c387
Isabella Skořepová <isabella@skorepova.info>
2023-11-12 15:25:20 +0100

    kdl + update

--- vscode/extensions.nix
+++ vscode/extensions.nix
@@ -1,112 +1,113 @@
 # nu ~/nixos/vscode/gen-extensions.nu
-{ extensions = [
-  {
-    name = "vscode-zipfs";
-    publisher = "arcanis";
-    version = "3.0.0";
-    sha256 = "0wvrqnsiqsxb0a7hyccri85f5pfh9biifq4x2bllpl8mg79l5m68";
-  }
-  {
-    name = "Nix";
-    publisher = "bbenoist";
-    version = "1.0.1";
-    sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
-  }
-  {
-    name = "vscode-tailwindcss";
-    publisher = "bradlc";
-    version = "0.11.29";
-    sha256 = "024fq21q0y7gm6wwwphqh5pyzfp5rahq27hkabn4qixd529zg4i5";
-  }
-  {
-    name = "vscode-eslint";
-    publisher = "dbaeumer";
-    version = "2.4.2";
-    sha256 = "1g5mavks3m4fnn7wav659rdnd9f3lp7r96g8niad4g1vaj4xm23q";
-  }
-  {
-    name = "EditorConfig";
-    publisher = "EditorConfig";
-    version = "0.16.4";
-    sha256 = "0fa4h9hk1xq6j3zfxvf483sbb4bd17fjl5cdm3rll7z9kaigdqwg";
-  }
-  {
-    name = "prettier-vscode";
-    publisher = "esbenp";
-    version = "10.1.0";
-    sha256 = "01s0vi2h917mqfpdrhqhp2ijwkibw95yk2js0l587wvajbbry2s9";
-  }
-  {
-    name = "kdl";
-    publisher = "kdl-org";
-    version = "1.3.1";
-    sha256 = "1a302y4xkqng5pbiyzxlr3mpl1r9g4813m14gzzjh6wsmj3z4rni";
-  }
-  {
-    name = "direnv";
-    publisher = "mkhl";
-    version = "0.15.2";
-    sha256 = "06lp4qgnksklgc6nvx1l9z38y7apbx0a6v886nd15aq9rq8my0ka";
-  }
-  {
-    name = "python";
-    publisher = "ms-python";
-    version = "2023.21.13131007";
-    sha256 = "0w1hlc2v504vd2i7vgdqjg8kal6fqymlrxs39mkizj4i9warfqja";
-  }
-  {
-    name = "vscode-pylance";
-    publisher = "ms-python";
-    version = "2023.11.12";
-    sha256 = "1sz5jdnc52h8vskcb1v8dk1w8hx3l8lfj8fd4428xc23g8z5rxz3";
-  }
-  {
-    name = "prisma";
-    publisher = "Prisma";
-    version = "5.5.2";
-    sha256 = "0jn11x1mv2lf1rxb2cq9jdi4f6r1y6wzvzr3avy4mfnbc4aw8xyd";
-  }
-  {
-    name = "rewrap";
-    publisher = "stkb";
-    version = "17.8.0";
-    sha256 = "1y168ar01zxdd2x73ddsckbzqq0iinax2zv3d95nhwp9asjnbpgn";
-  }
-  {
-    name = "even-better-toml";
-    publisher = "tamasfe";
-    version = "0.19.2";
-    sha256 = "0q9z98i446cc8bw1h1mvrddn3dnpnm2gwmzwv2s3fxdni2ggma14";
-  }
-  {
-    name = "vscode-nushell-lang";
-    publisher = "TheNuProjectContributors";
-    version = "1.7.1";
-    sha256 = "0iv1x0djyc5hsbd7f8afx4gy11dkflafjyr4lr83ghdbnvr1jn96";
-  }
-  {
-    name = "vscode-mdx";
-    publisher = "unifiedjs";
-    version = "1.4.0";
-    sha256 = "0y65z0w3569375szbxd1cfl975w7s34vdrfk5m14ciwk0b8smama";
-  }
-  {
-    name = "vscode-icons";
-    publisher = "vscode-icons-team";
-    version = "12.6.0";
-    sha256 = "0d0p2v2dl8g4x5sv5cj85rpjpgq2mmk3dxdnqqfkvfz2lf2n2aab";
-  }
-  {
-    name = "gitblame";
-    publisher = "waderyan";
-    version = "10.5.1";
-    sha256 = "119rf52xnxz0cwvvjjfc5m5iv19288cxz33xzr79b67wyfd79hl9";
-  }
-  {
-    name = "change-case";
-    publisher = "wmaurer";
-    version = "1.0.0";
-    sha256 = "0dxsdahyivx1ghxs6l9b93filfm8vl5q2sa4g21fiklgdnaf7pxl";
-  }
-];
+{
+  extensions = [
+    {
+      name = "vscode-zipfs";
+      publisher = "arcanis";
+      version = "3.0.0";
+      sha256 = "0wvrqnsiqsxb0a7hyccri85f5pfh9biifq4x2bllpl8mg79l5m68";
+    }
+    {
+      name = "Nix";
+      publisher = "bbenoist";
+      version = "1.0.1";
+      sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+    }
+    {
+      name = "vscode-tailwindcss";
+      publisher = "bradlc";
+      version = "0.11.30";
+      sha256 = "1xbcvs2i079xiah679cjczansfrgr2cp44xvydq04ndv1fyp4b6l";
+    }
+    {
+      name = "vscode-eslint";
+      publisher = "dbaeumer";
+      version = "2.4.2";
+      sha256 = "1g5mavks3m4fnn7wav659rdnd9f3lp7r96g8niad4g1vaj4xm23q";
+    }
+    {
+      name = "EditorConfig";
+      publisher = "EditorConfig";
+      version = "0.16.4";
+      sha256 = "0fa4h9hk1xq6j3zfxvf483sbb4bd17fjl5cdm3rll7z9kaigdqwg";
+    }
+    {
+      name = "prettier-vscode";
+      publisher = "esbenp";
+      version = "10.1.0";
+      sha256 = "01s0vi2h917mqfpdrhqhp2ijwkibw95yk2js0l587wvajbbry2s9";
+    }
+    {
+      name = "kdl";
+      publisher = "kdl-org";
+      version = "1.3.1";
+      sha256 = "1a302y4xkqng5pbiyzxlr3mpl1r9g4813m14gzzjh6wsmj3z4rni";
+    }
+    {
+      name = "direnv";
+      publisher = "mkhl";
+      version = "0.15.2";
+      sha256 = "06lp4qgnksklgc6nvx1l9z38y7apbx0a6v886nd15aq9rq8my0ka";
+    }
+    {
+      name = "python";
+      publisher = "ms-python";
+      version = "2023.21.13141007";
+      sha256 = "1ba1r8hmpqahcafky5scnc2clyc1ja0g8xkhvmi295gc28s53rda";
+    }
+    {
+      name = "vscode-pylance";
+      publisher = "ms-python";
+      version = "2023.11.12";
+      sha256 = "1sz5jdnc52h8vskcb1v8dk1w8hx3l8lfj8fd4428xc23g8z5rxz3";
+    }
+    {
+      name = "prisma";
+      publisher = "Prisma";
+      version = "5.5.2";
+      sha256 = "0jn11x1mv2lf1rxb2cq9jdi4f6r1y6wzvzr3avy4mfnbc4aw8xyd";
+    }
+    {
+      name = "rewrap";
+      publisher = "stkb";
+      version = "17.8.0";
+      sha256 = "1y168ar01zxdd2x73ddsckbzqq0iinax2zv3d95nhwp9asjnbpgn";
+    }
+    {
+      name = "even-better-toml";
+      publisher = "tamasfe";
+      version = "0.19.2";
+      sha256 = "0q9z98i446cc8bw1h1mvrddn3dnpnm2gwmzwv2s3fxdni2ggma14";
+    }
+    {
+      name = "vscode-nushell-lang";
+      publisher = "TheNuProjectContributors";
+      version = "1.7.1";
+      sha256 = "0iv1x0djyc5hsbd7f8afx4gy11dkflafjyr4lr83ghdbnvr1jn96";
+    }
+    {
+      name = "vscode-mdx";
+      publisher = "unifiedjs";
+      version = "1.5.0";
+      sha256 = "1vq2b3krvxccskh417zmz3inniqr1scz53bg49dp1q14axqy42sk";
+    }
+    {
+      name = "vscode-icons";
+      publisher = "vscode-icons-team";
+      version = "12.6.0";
+      sha256 = "0d0p2v2dl8g4x5sv5cj85rpjpgq2mmk3dxdnqqfkvfz2lf2n2aab";
+    }
+    {
+      name = "gitblame";
+      publisher = "waderyan";
+      version = "10.5.1";
+      sha256 = "119rf52xnxz0cwvvjjfc5m5iv19288cxz33xzr79b67wyfd79hl9";
+    }
+    {
+      name = "change-case";
+      publisher = "wmaurer";
+      version = "1.0.0";
+      sha256 = "0dxsdahyivx1ghxs6l9b93filfm8vl5q2sa4g21fiklgdnaf7pxl";
+    }
+  ];
 }
