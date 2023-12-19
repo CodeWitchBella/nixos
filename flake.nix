708da19e86d8ec9274147b2ec34932a6c84057ee
Isabella Skořepová <isabella@skorepova.info>
2023-12-19 13:20:17 +0100

    working asahi

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin.url = "github:lnl7/nix-darwin";
