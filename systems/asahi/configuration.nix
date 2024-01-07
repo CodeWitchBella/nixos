3e8617a2af88f71ea0bd1c2741081a03e293749b
Isabella Skořepová <isabella@skorepova.info>
2024-01-07 13:09:43 +0100

    invert fnmode

--- systems/asahi/configuration.nix
+++ systems/asahi/configuration.nix
@@ -20,7 +20,7 @@
   boot.extraModprobeConfig = ''
     options hid_apple swap_opt_cmd=1
     options hid_apple swap_fn_leftctrl=1
-    options hid_apple fnmode=1
+    options hid_apple fnmode=2
   '';
 
   sound.enable = true;
