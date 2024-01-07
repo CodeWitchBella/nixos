aa0cd0612eebbbd4448e16252aa70e560849f49e
Isabella Skořepová <isabella@skorepova.info>
2024-01-07 13:20:51 +0100

    remove input-remapper

--- systems/asahi/home.nix
+++ systems/asahi/home.nix
@@ -10,6 +10,4 @@
   };
 in
   lib.recursiveUpdate shared {
-    home.file.".config/input-remapper-2/config.json".source = ./input-remapper/config.json;
-    home.file.".config/input-remapper-2/presets/Apple MTP keyboard/normal_order.json".source = ./input-remapper/normal_order.json;
   }
