c291d0f2c1471837ac71bfeeedc9ecc294f99996
Isabella Skořepová <isabella.skorepova@draslovka.com>
2024-02-05 17:06:49 +0100

    do not replicate deleted tags

--- systems/shared/git.nix
+++ systems/shared/git.nix
@@ -10,8 +10,8 @@
     user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEGS4V/SauPK+C9moGX5gscGYYPNV5E6QNUyaZrL1eg0";
     commit.gpgsign = true;
     gpg.format = "ssh";
-    alias.frp = "!bash -c \"git fetch --prune ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`; git push --force\"";
-    alias.fr = "!bash -c \"git fetch --prune ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`\"";
+    alias.frp = "!bash -c \"git fetch --prune --tags ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`; git push --force\"";
+    alias.fr = "!bash -c \"git fetch --prune --tags ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`\"";
     rebase.autostash = true;
     pull.rebase = true;
     "gpg \"ssh\"".program = "${pkgs.openssh}/bin/ssh-keygen";
