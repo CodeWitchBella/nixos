8d6871d4027921b652af6dce21ccd5f0d42dd5c0
Isabella Skořepová <isabella.skorepova@draslovka.com>
2024-02-05 17:57:29 +0100

    git alias: ruff

--- systems/shared/git.nix
+++ systems/shared/git.nix
@@ -12,6 +12,23 @@
     gpg.format = "ssh";
     alias.frp = "!bash -c \"git fetch --prune --tags ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`; git push --force\"";
     alias.fr = "!bash -c \"git fetch --prune --tags ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`\"";
+    alias.ruff = ''!bash -c "
+        set -xe
+        if [ -z \"`git status --porcelain`\" ]; then
+          if ruff format --check && ruff check; then
+            echo \"Everything ok\"
+          else
+            ruff format
+            ruff check --fix
+            git commit -a -m ruff
+            echo \"ruffed some feathers\"
+          fi
+        else
+          echo \"not ready for ruff\"
+          exit 1
+        fi
+      "
+    '';
     rebase.autostash = true;
     pull.rebase = true;
     "gpg \"ssh\"".program = "${pkgs.openssh}/bin/ssh-keygen";
