deb641e038da62926a63a5bd0d6d3381f5cc7d6e
Isabella Skořepová <isabella@skorepova.info>
2024-02-12 11:01:17 +0100

    widevine overlay

--- systems/asahi/configuration.nix
+++ systems/asahi/configuration.nix
@@ -13,6 +13,10 @@
     ../personal/configuration.nix
   ];
 
+  nixpkgs.overlays = [
+    (import ./widevine.nix)
+  ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
