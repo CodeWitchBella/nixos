40f0932ad15992d0a538816a3f96fbf328eb98ee
Isabella Skořepová <isabella.skorepova@draslovka.com>
2024-03-01 14:19:16 +0100

    ts formatting

--- vscode/vscode.nix
+++ vscode/vscode.nix
@@ -50,6 +50,7 @@
     "git.confirmSync" = false;
     "git.autofetch" = true;
     "[typescriptreact]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+    "[typescript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
     "[javascript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
     "[html]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
     "editor.rulers" = [80 120];
