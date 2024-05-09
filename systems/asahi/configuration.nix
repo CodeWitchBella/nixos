2214320b64d43274176972411e7edcb98ee3a702
Isabella Skořepová <isabella.skorepova@draslovka.com>
2024-05-09 12:26:41 +0200

    lix substituters everywhere

--- systems/asahi/configuration.nix
+++ systems/asahi/configuration.nix
@@ -13,14 +13,6 @@
     ../personal/configuration.nix
   ];
 
-  nix.settings.extra-substituters = [
-    "https://cache.lix.systems"
-  ];
-
-  nix.settings.trusted-public-keys = [
-    "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-  ];
-
   nixpkgs.overlays = [
     (import ./widevine.nix)
   ];
--- systems/shared/configuration.nix
+++ systems/shared/configuration.nix
@@ -12,6 +12,14 @@
     options = "--delete-older-than 60d";
   };
 
+  nix.settings.extra-substituters = [
+    "https://cache.lix.systems"
+  ];
+
+  nix.settings.trusted-public-keys = [
+    "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+  ];
+
   system.autoUpgrade = {
     enable = false;
     flake = "${config.users.users.isabella.home}/nixos";
