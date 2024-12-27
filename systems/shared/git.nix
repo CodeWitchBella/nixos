39ca9cfff50fa9fce979ad667a620ac1f90d4dcf
Isabella Skořepová <isabella.skorepova@draslovka.com>
2024-12-27 11:13:29 +0100

    git prune

--- systems/shared/git.nix
+++ systems/shared/git.nix
@@ -40,6 +40,15 @@
         fi
       ' - \
     '';
+    alias.prune = ''
+      !bash -c '
+        set -e
+        git fetch -p
+        for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do
+          git branch -D $branch
+        done
+      ' - \
+    '';
     alias.fr = "!bash -c \"git fetch --prune --tags ; git rebase `git symbolic-ref refs/remotes/origin/HEAD --short`\"";
     alias.ruff = ''
       !bash -c "
