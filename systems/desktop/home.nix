f4cc5cf784bf3230706d49b8a2dc4044fa45ecd6
Isabella Skořepová <isabella@skorepova.info>
2025-04-04 22:16:44 +0200

    plasma

--- systems/desktop/home.nix
+++ systems/desktop/home.nix
@@ -15,21 +15,21 @@ lib.recursiveUpdate shared {
     user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFrYVxQiKKIzGqLIO+6w6qA1d+E9vR2bFLW0EuT4e6zA";
   };
 
-  programs.plasma.input.keyboard.layouts = [
-    {
-      layout = "us";
-    }
-    {
-      layout = "cz";
-      variant = "qwerty";
-    }
-  ];
-  programs.plasma.kwin.effects.fallApart.enable = true;
-  programs.plasma.kwin.nightLight.mode = "location";
-  programs.plasma.kwin.nightLight.location.latitude = "50.0835494";
-  programs.plasma.kwin.nightLight.location.longitude = "14.4341414";
-  programs.plasma.kwin.nightLight.temperature.night = 4500;
-  programs.plasma.kwin.scripts.polonium.enable = true;
+  # programs.plasma.input.keyboard.layouts = [
+  #   {
+  #     layout = "us";
+  #   }
+  #   {
+  #     layout = "cz";
+  #     variant = "qwerty";
+  #   }
+  # ];
+  # programs.plasma.kwin.effects.fallApart.enable = true;
+  # programs.plasma.kwin.nightLight.mode = "location";
+  # programs.plasma.kwin.nightLight.location.latitude = "50.0835494";
+  # programs.plasma.kwin.nightLight.location.longitude = "14.4341414";
+  # programs.plasma.kwin.nightLight.temperature.night = 4500;
+  # programs.plasma.kwin.scripts.polonium.enable = true;
 
   home.file.".config/powerdevilrc" = {
     text = ''
